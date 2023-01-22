f4168289513e606b8af1ed66534f0bf93aeae136
Sergey Bilibin <s.bilibin2017@yandex.ru>
2023-01-22 16:58:47 +0300

    Сделана django админка

---
+++ movies_admin/config/settings.py
@@ -0,0 +1,45 @@
+'''Модуль с настройками django проекта.'''
+
+# импорт библиотек
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from split_settings.tools import include
+
+# корень проекта
+BASE_DIR = Path(__file__).resolve().parent.parent
+# загрузка переенных окружения
+load_dotenv(BASE_DIR / '.env')
+# секрет джанго
+SECRET_KEY = os.getenv('SECRET_KEY')
+# режим запуска
+DEBUG = os.getenv('DEBUG') == 'True'
+# доступные порты в режиме деплоя (DEBUG=False)
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+# для панели дебага
+INTERNAL_IPS = os.getenv('INTERNAL_IPS').split(',')
+# контроллеры
+ROOT_URLCONF = 'config.urls'
+# вебсервер
+WSGI_APPLICATION = 'config.wsgi.application'
+# компоненты, выведенные в отдельный модуль
+include(
+    'components/databases.py',
+    'components/installed_apps.py',
+    'components/middleware.py',
+    'components/templates.py',
+    'components/auth_password_validators.py',
+)
+# язык
+LANGUAGE_CODE = 'ru-RU'
+# время
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+# директория со стотическими файлами
+STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# директория с локализацией
+LOCALE_PATHS = ['movies/locale']
---
+++ movies_admin/movies/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from .models import Filmwork, FilmworkGenre, FilmworkPerson, Genre, Person
+
+
+def custom_titled_filter(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+            return instance
+    return Wrapper
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'updated_at')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'updated_at')
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = FilmworkGenre
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = FilmworkPerson
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline,)
+    # Отображение полей в списке
+    list_display = ('title', 'description', 'type', 'rating', 'creation_date')
+    # фильтры
+    list_filter = ('type', 'creation_date',
+                   ('genres__name', custom_titled_filter(_('Genre'))),
+                   ('persons__full_name', custom_titled_filter(_('Person'))))
+    # поиск
+    search_fields = ('title', 'description', 'id')
